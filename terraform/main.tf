9ffebf63d0b00a5b4a96bd51740fc4cd68fd0ded
mazcot-devops <natthawut.rao@gmail.com>
2023-11-17 21:56:52 +0700

    test cicd

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,42 @@ provider "aws" {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "ml-eks-cluster"
+  subnets         = ["subnet-1", "subnet-2", "subnet-3"]
+  vpc_id          = "vpc-01234567890abcdef"
   cluster_version = "1.25"
-
-  subnets = {
-    private = ["subnet-1", "subnet-2", "subnet-3"]
-    public  = ["subnet-4", "subnet-5", "subnet-6"]
-  }
-
-  node_groups = {
+  node_groups     = {
     eks_nodes = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
     }
   }
+}
+
+resource "aws_iam_role" "ml_app_role" {
+  name = "ml-app-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks-fargate-pods.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role       = aws_iam_role.ml_app_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_access_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+  role       = aws_iam_role.ml_app_role.name
 }
