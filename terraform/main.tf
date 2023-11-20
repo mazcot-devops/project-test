e6da7d9aa34147a3243494b00ba190d19e64969d
mazcot-devops <natthawut.rao@gmail.com>
2023-11-20 21:48:12 +0700

    change values

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,22 +3,55 @@ provider "aws" {
 }
 
 module "eks" {
-  source            = "terraform-aws-modules/eks/aws"
-  cluster_name      = "ml-eks-cluster"
-  subnets           = ["subnet-1", "subnet-2", "subnet-3"]  
-  vpc_id            = "vpc01234567890abcdef"                          
-  cluster_version   = "1.25"
-  worker_groups     = {
+  source         = "terraform-aws-modules/eks/aws"
+  cluster_name   = "ml-eks-cluster"
+  subnets        = ["subnet-1", "subnet-2", "subnet-3"]
+  vpc_id         = "vpc-01234567890abcdef"
+  cluster_version = "1.25"
+
+  worker_groups = {
     eks_nodes = {
       desired_capacity = 2
       max_capacity     = 3
-      min_capacity     = 1
+      mln_capacity     = 1
     }
   }
 }
 
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "s3-access-policy"
+  description = "IAM policy for S3 access"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:PutObject"],
+        Resource = ["arn:aws:s3:::my-web-assets/*"],
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "sqs_access_policy" {
+  name        = "sqs-access-policy"
+  description = "IAM policy for SQS access"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["sqs:SendMessage", "sqs:ReceiveMessage", "sqs:DeleteMessage"],
+        Resource = ["arn:aws:sqs:ap-southeast-1:123456789123:lms-import-data"],
+      },
+    ],
+  })
+}
+
 resource "aws_iam_role" "ml_app_role" {
-  name = "ml-app-role"
+  name             = "ml-app-role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -36,11 +69,11 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  policy_arn = aws_iam_policy.s3_access_policy.arn
   role       = aws_iam_role.ml_app_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "sqs_access_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+  policy_arn = aws_iam_policy.sqs_access_policy.arn
   role       = aws_iam_role.ml_app_role.name
 }
